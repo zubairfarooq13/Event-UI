09368944fd40a825c88d03f82508a823cd51c9d0
Zubair Farooq <43909324+zubairfarooq13@users.noreply.github.com>
2025-11-19 01:55:39 +0500

    Add vendor login page and route
    Introduces a new VendorLogin component for venue users, including a login form and Google login button. Adds the /login/vendor route to App.js to support vendor authentication.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LandingPage from './components/landing/LandingPage';
 import HomeSearch from './components/home/HomeSearch';
 import LoginLanding from './components/auth/LoginLanding';
 import ClientLogin from './components/auth/ClientLogin';
+import VendorLogin from './components/auth/VendorLogin';
 import ListYourVenue from './components/landing/ListYourVenue';
 import Signup from './components/auth/Signup';
 import ProfilePage from './components/profile/ProfilePage';
@@ -24,6 +25,7 @@ function App() {
             <Route path="/search" element={<HomeSearch />} />
             <Route path="/login" element={<LoginLanding />} />
             <Route path="/login/user" element={<ClientLogin />} />
+            <Route path="/login/vendor" element={<VendorLogin />} />
             <Route path="/list-your-venue" element={<ListYourVenue />} />
             <Route path="/register" element={<Signup />} />
             <Route path="/signup/user" element={<Signup />} />
